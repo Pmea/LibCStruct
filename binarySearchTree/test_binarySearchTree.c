fe4dd9a7d430841bb77518035c51b66537d0816f
krumdine <turonjeremy@hotmail.fr>
2014-06-05 14:21:59 +0200

    ajout d'un test pour les binary search tree.

--- binarySearchTree/test_binarySearchTree.c
+++ binarySearchTree/test_binarySearchTree.c
@@ -1,5 +1,27 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+#include "binarySearchTree.h"
 
 int main() {
+
+	binarySearchTree bst = create_BST(50) ;
+	for(int i = 0 ; i <= 100 ; i+= 10) {
+		if(i != 50)
+			insertValue_BST(bst,i) ;
+	}
+
+	deleteValue_BST(bst,20) ;
+	for(int i = 0 ; i <= 100 ; i+=10) {
+		if(i == 20) {
+			if(findValue_BST(bst,i) != NULL)
+				printf("Error %d\n",i);
+		} else {
+		if(findValue_BST(bst,i) == NULL)
+			printf("Error %d\n",i) ;
+		}
+	}
+
+	destroy_BST(bst) ;
 	return EXIT_SUCCESS ;
 }
